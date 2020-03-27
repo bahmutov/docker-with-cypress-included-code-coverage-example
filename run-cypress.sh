f0c06a13b3ab73af31839f89262af0d48722cd32
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-27 16:30:23 -0400

    add DEBUG note

--- run-cypress.sh
+++ run-cypress.sh
@@ -1,2 +1,6 @@
+# if you want to see debug logs from Cypress
+# or from code coverage plugin, set DEBUG environment variable
+# for example DEBUG=code-coverage ./run-cypress.sh
 docker run -it -v $PWD:/e2e -w /e2e \
+  -e DEBUG \
   cypress/included:4.2.0
